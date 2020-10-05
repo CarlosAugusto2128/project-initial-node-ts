d82cf62bd50baecd94e8083cf3f54d390ef645c6
Carlos Augusto <carlosaugustoss2128@gmail.com>
2020-10-05 15:49:30 -0300

    Update: Learning about Interface with types differents of Typing.

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,18 @@
 import { Request, Response } from "express";
+import createUser from "./services/CreateUser";
 
 export function helloWorld(req: Request, res: Response) {
+  const user = createUser({
+    name: "Teste Jose",
+    email: "teste@gmail.com",
+    password: "123456",
+    techs: [
+      "Node.js",
+      "ReactJS",
+      "ReactNative",
+      { title: "Javascript", experience: 100 },
+    ],
+  });
+
   return res.json({ msg: "Hello World" });
 }
